37aedea3bdf9b5ecb24c87021afb3f0e1a2dbb70
Jonathon M. Abbott <jonathona@everydayhero.com.au>
2015-05-27 09:34:40 +1000

    WIP config

--- lib/git_manage_install.rb
+++ lib/git_manage_install.rb
@@ -1,4 +1,5 @@
 require "git_manage_install/version"
+require "git_manage_install/config"
 
 module GitManageInstall
   # Your code goes here...
--- lib/git_manage_install/config.rb
+++ lib/git_manage_install/config.rb
@@ -1,8 +1,17 @@
 require "xdg"
+require "yaml"
 
 module GitManageInstall
   class Config
     include XDG::BaseDir::Mixin
+
+    def configuration
+      @configuration ||= YAML.load_file config_file
+    end
+
+    def config_file
+      config.home.find("packages.yml") or raise "missing config file packages.yml"
+    end
 
     def subdirectory
       "git-manage-install"
